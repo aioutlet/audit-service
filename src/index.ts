de3a397f7900993838fd6eea5dd3fbfa59528654
prasadhonrao <honrao.prasad@gmail.com>
2025-09-02 16:20:13 +0100

    refactor(tracing): streamline tracing initialization by removing OpenTelemetry setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 // Initialize tracing FIRST - this must be the very first import
-import { initializeTracing } from './observability/tracing/setup.js';
-
-// Initialize OpenTelemetry before anything else
-initializeTracing();
+import './tracing-init.js';
 
 import AuditServiceApp from './app.js';
 
